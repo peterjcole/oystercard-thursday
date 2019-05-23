ca4288b1b44079749bc7e07436765a3725258fb3
JAYDA KADIR <jydkdr@gmail.com>
2019-05-23 16:20:13 +0100

    Refactor oystercard to use journey class

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -7,11 +7,14 @@ class OysterCard
   DEFAULT_LIMIT = 90
   MINIMUM = 1
   FARE = 2
+  PENALTY_FARE = 6
 
-  def initialize
+  def initialize( journey_class=Journey )
     @balance = 0
     @entry_station =  nil
     @journeys = []
+    @journey_class = journey_class
+    @current_journey = journey_class.new
   end
 
   def top_up(money)
@@ -23,23 +26,24 @@ class OysterCard
   def touch_in(station)
     raise "You need a minimum of £1" if @balance < MINIMUM
     @entry_station = station
+    @current_journey.start(station)
   end
 
-  def touch_out(fare = FARE, station)
-    deduct(fare)
+  def touch_out(station)
     @exit_station = station
     store_trips
     @entry_station = nil
+    @current_journey.stop(station)
+  end
+
+  def deduct_fare
+    @current_journey.complete? ? @balance -= FARE : @balance -= PENALTY_FARE
   end
 
   private
 
-  def deduct(fare)
-    @balance -= fare
-  end
-
   def store_trips
-    @journeys.push({entry_station: @entry_station, exit_station: @exit_station})
+    @journeys.push(@current_journey)
   end
 
 
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -5,9 +5,9 @@ require './lib/oyster_card'
 
 describe OysterCard do
 
-  let(:entry_station) { double(:station) }
-  let(:exit_station) { double(:station) }
-  let(:oyster_card) { OysterCard.new }
+  let(:entry_station) { double(:station, :name => "Barbican") }
+  let(:exit_station) { double(:station, :name => "Camden Town") }
+  let(:oyster_card) { OysterCard.new(journey_class) }
 
   it 'can get the current balance on the oyster card' do
     oyster_card.top_up(10)
@@ -23,20 +23,21 @@ describe OysterCard do
     expect{oyster_card.top_up(1)}.to raise_error "Top up limit exceeded"
   end
 
-  it 'can deduct a fare from the card' do
-    oyster_card.top_up(OysterCard::DEFAULT_LIMIT)
-    oyster_card.touch_out(90, exit_station)
-    expect(oyster_card.balance).to eq(0)
-  end
+  # it 'can deduct a fare from the card' do
+  #   oyster_card.top_up(OysterCard::DEFAULT_LIMIT)
+  #   oyster_card.touch_out(90, exit_station)
+  #
+  #   expect(oyster_card.balance).to eq(0)
+  # end
 
   it 'needs a minimum of £1' do
     expect{oyster_card.touch_in(entry_station)}.to raise_error "You need a minimum of £1"
   end
 
-  it 'will deduct a fare when you complete a journey' do
-    oyster_card.top_up(10)
-    expect{ oyster_card.touch_out(2) }.to change { oyster_card.balance }.by(-2)
-  end
+  # it 'will deduct a fare when you complete a journey' do
+  #   oyster_card.top_up(10)
+  #   expect{ oyster_card.touch_out(2) }.to change { oyster_card.balance }.by(-2)
+  # end
 
   it 'remembers the entry station after touch in' do
     oyster_card.top_up(10)
@@ -54,7 +55,7 @@ describe OysterCard do
   it 'remembers the exit station on touch out' do
     oyster_card.top_up(10)
     oyster_card.touch_in(entry_station)
-    oyster_card.touch_out(2, exit_station)
+    oyster_card.touch_out(exit_station)
     expect(oyster_card.exit_station).to eq(exit_station)
   end
 
@@ -62,24 +63,41 @@ describe OysterCard do
     expect(oyster_card.journeys).to eq([])
   end
 
+  it 'starts a journey when you touch in' do
+    oyster_card.top_up(10)
+    expect(journey).to receive(:start).with(entry_station)
+    oyster_card.touch_in(entry_station)
+  end
+
+  it 'ends a journey when you touch out' do
+    oyster_card.top_up(10)
+    expect(journey).to receive(:stop).with(exit_station)
+    oyster_card.touch_out(exit_station)
+  end
+
   it 'stores a list of journeys' do
     oyster_card.top_up(10)
     oyster_card.touch_in(entry_station)
-    oyster_card.touch_out(2, exit_station)
-    expect(oyster_card.journeys).to eq([{:entry_station => entry_station, :exit_station => exit_station}])
+    oyster_card.touch_out(exit_station)
+    expect(oyster_card.journeys).to eq()
   end
 
-  context '#fare' do
-    # it 'deducts the minimum fare when journey is complete' do
-    #   pending()
-    #   # journey.start(entry_station)
-    #   # journey.stop(exit_station)
-    #   # expect(journey.fare).to be
-    # end
+  let(:journey) { double(:journey, :complete? => true).as_null_object }
+  let(:journey_class) { double(:journey_class, :new => journey )}
 
-    # it 'deducts the penalty fare when journey is incomplete' do
-    #   pending()
-    # end
+  context '#fare' do
+    it 'deducts the minimum fare when journey is complete' do
+      oyster_card.top_up(10)
+      oyster_card.deduct_fare
+      expect{ oyster_card.deduct_fare }.to change { oyster_card.balance }.by(-2)
+    end
+
+    it 'deducts the penalty fare when journey is incomplete' do
+      allow(journey).to receive(:complete?).and_return(false)
+      oyster_card.top_up(10)
+      oyster_card.deduct_fare
+      expect{ oyster_card.deduct_fare }.to change { oyster_card.balance }.by(-6)
+    end
   end
 
 end
